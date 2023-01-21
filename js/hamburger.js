efd91124bd118a4947ae4271e742a60bc194812d
joseGuaymas <joseguaymas@hotmail.com>
2023-01-21 18:39:54 -0300

    Desarrollé un cambio en el menú de navegación cuando la página se muestre en pantallas de un ancho superior a 1024px.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,7 @@
 const hamburguer = document.querySelector("#hamburguer");
 const close = document.querySelector("#close");
 const nav = document.querySelector("nav");
+const navlist = document.querySelector('#navlist')
 
 hamburguer.addEventListener('click', () => {
     nav.classList.toggle("mostrar-menu")
@@ -18,4 +19,38 @@ window.addEventListener('click', (e)=> {
         hamburguer.classList.toggle('hidden')
     }
 });
+
+function fullMenu() {  
+    close.classList.add('hidden')
+    hamburguer.classList.add('hidden')
+    nav.classList.add('hidden')
+    navlist.classList.remove('hidden')
+    navlist.classList.add('navlist-fullscreen')
+}
+
+function hideMenu() {  
+    close.classList.remove('hidden')
+    hamburguer.classList.remove('hidden')
+    nav.classList.remove('hidden')
+    navlist.classList.remove('navlist-fullscreen')
+    navlist.classList.add('hidden')
+}
+
+console.log(window.innerWidth)
+
+if (window.innerWidth < 1024) {
+    hideMenu()
+    console.log('ok')
+} else {
+    fullMenu()
+}
+    
+    
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 1024) {
+        hideMenu()
+    } else {
+        fullMenu()
+    }
+})
 
